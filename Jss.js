1cdcce36aa212d6083a9346ba187ab5a3ab9982b
Venkat Raja CH <yvenkatraja59@gmail.com>
2019-10-02 20:03:11 +0530

    Added two file one is Html and the other one is the supportive Java Script file and from here we will be working on, all the process is done through terminal by using git commands and my first practical experience of using git command in a terminal as complete beginer

---
+++ Jss.js
@@ -0,0 +1,26 @@
+console.log('Hello World !!!');
+
+/* 
+Variables and data types
+*/
+var firstName = 'Venkat Raja';
+var lastName = 'CH';
+
+var age = 20;
+
+var bool = true;
+
+var valid;
+
+console.log(firstName);
+console.log(lastName);
+
+console.log(age);
+
+console.log(bool);
+
+console.log(valid);
+
+valid = 'Yes We Can';
+
+console.log(valid);
